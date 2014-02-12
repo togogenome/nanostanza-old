8533ae5cda717721326c65d638e08a4f93ac7577
Shuichi Kawashima <shuichi.kawashima@gmail.com>
2014-02-12 20:13:55 +0900

    Change the endpoint of organism_ph_nano_stanza

--- organism_ph_nano_stanza/stanza.rb
+++ organism_ph_nano_stanza/stanza.rb
@@ -1,6 +1,6 @@
 class OrganismPhNanoStanza < TogoStanza::Stanza::Base
   property :ph_info do |tax_id|
-    query("http://ep.dbcls.jp/sparql7upd2", <<-SPARQL.strip_heredoc)
+    query("http://togostanza.org/sparql", <<-SPARQL.strip_heredoc)
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       PREFIX mpo: <http://purl.jp/bio/01/mpo#>
       PREFIX tax: <http://identifiers.org/taxonomy/>
