76edc51000b8a7a31eb810423b28396f672d9145
Toshiaki Katayama <ktym@dbcls.jp>
2014-02-12 20:14:05 +0900

    Fixed SPARQL endpoint

--- organism_gc_nano_stanza/stanza.rb
+++ organism_gc_nano_stanza/stanza.rb
@@ -8,7 +8,7 @@ class OrganismGcNanoStanza < TogoStanza::Stanza::Base
   property :gc do |tax_id|
     # http://localhost:9292/stanza/organism_gc_nano?tax_id=192222
 
-    results = query('http://ep.dbcls.jp/sparql-bh1313', <<-SPARQL.strip_heredoc)
+    results = query('http://togostanza.org/sparql', <<-SPARQL.strip_heredoc)
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       PREFIX tax: <http://identifiers.org/taxonomy/>
       PREFIX insdc: <http://ddbj.nig.ac.jp/ontologies/sequence#>
