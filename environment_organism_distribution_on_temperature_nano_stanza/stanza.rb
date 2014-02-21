b112afd14d48d39dba83ee6a295acb299ce61eaa
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-02-21 11:39:02 +0900

    Cosmetic

--- environment_organism_distribution_on_temperature_nano_stanza/stanza.rb
+++ environment_organism_distribution_on_temperature_nano_stanza/stanza.rb
@@ -1,53 +1,4 @@
 class EnvironmentOrganismDistributionOnTemperatureNanoStanza < TogoStanza::Stanza::Base
-
-  def find_category(hash)
-    opt_temp = 0.0
-    if hash.key?(:l)
-      case hash[:l]
-      when "Mesophile", "Psychrophile", "Thermophile"
-        return hash[:l].to_sym
-      when "Hyperthermophile"
-        return :Thermophile
-      else
-        return :NA
-      end
-    elsif hash.key?(:opt_temp) || hash.key?(:opt_temp_brc)
-      hash.key?(:opt_temp) ? opt_temp = hash[:opt_temp].to_f
-                           : opt_temp = hash[:opt_temp_brc].to_f
-      if opt_temp >= 20.0 && opt_temp <= 45.0
-        return :Mesophile
-      elsif opt_temp > 45.0
-        return :Thermophile
-      elsif opt_temp < 10.0
-        return :Psychrophile
-      else
-        return :NA
-      end
-    elsif hash.key?(:min_temp) && hash.key?(:max_temp)
-      if hash[:min_temp].to_f >= 20.0 && hash[:max_temp].to_f <= 45.0
-        return :Mesophile
-      elsif hash[:min_temp].to_f >= 45.0
-        return :Thermophile
-      elsif hash[:max_temp].to_f < 10.0
-        return :Psychrophile
-      else
-        return :NA
-      end
-    elsif hash.key?(:min_temp_brc) && hash.key?(:max_temp_brc)
-      if hash[:min_temp_brc].to_f >= 20.0 && hash[:max_temp_brc].to_f <= 45.0
-        return :Mesophile
-      elsif hash[:min_temp_brc].to_f > 45.0
-        return :Thermophile
-      elsif hash[:max_temp_brc].to_f < 10.0
-        return :Psychrophile
-      else
-        return :NA
-      end
-    else
-      return :NA
-    end
-  end
-
   property :num_orgs_with_temperature_range do |meo_id|
     results = query("http://togostanza.org/sparql", <<-SPARQL.strip_heredoc)
       PREFIX mpo: <http://purl.jp/bio/01/mpo#>
@@ -83,7 +34,8 @@ class EnvironmentOrganismDistributionOnTemperatureNanoStanza < TogoStanza::Stanz
                 (!(?temperature = <NA>) || bound(?growth_temp_range)))
       }
     SPARQL
-    category2num = {:Mesophile => 0, :Thermophile => 0, :Psychrophile => 0} 
+
+    category2num = {Mesophile: 0, Thermophile: 0, Psychrophile: 0}
     results.each do |result|
       category = find_category(result)      
       if category2num.key?(category)
@@ -91,5 +43,44 @@ class EnvironmentOrganismDistributionOnTemperatureNanoStanza < TogoStanza::Stanz
       end
     end
     category2num
+  end
+
+  def find_category(hash)
+    opt_temp = 0.0
+    if hash.key?(:l)
+      category_by_label(hash[:l])
+    elsif hash.key?(:opt_temp) || hash.key?(:opt_temp_brc)
+      opt_temp = hash.key?(:opt_temp) ? hash[:opt_temp].to_f : hash[:opt_temp_brc].to_f
+      category_by_range(opt_temp, opt_temp)
+    elsif hash.key?(:min_temp) && hash.key?(:max_temp)
+      category_by_range(hash[:min_temp].to_f, hash[:max_temp].to_f)
+    elsif hash.key?(:min_temp_brc) && hash.key?(:max_temp_brc)
+      category_by_range(hash[:min_temp_brc].to_f, hash[:max_temp_brc].to_f)
+    else
+      :NA
+    end
+  end
+
+  def category_by_label(label)
+    case label
+    when "Mesophile", "Psychrophile", "Thermophile"
+      label.to_sym
+    when "Hyperthermophile"
+      :Thermophile
+    else
+      :NA
+    end
+  end
+
+  def category_by_range(min, max)
+    if min >= 20 && max <= 45
+      :Mesophile
+    elsif min > 45
+      :Thermophile
+    elsif max < 10
+      :Psychrophile
+    else
+      :NA
+    end
   end
 end
