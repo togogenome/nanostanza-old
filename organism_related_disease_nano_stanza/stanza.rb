df85a388b590507a606323f154f8909558420f2f
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-07-30 17:56:27 +0900

    リファクタリング
    - Query 内の BIND でエラーが出ていたが、そもそも BIND の必要は無かった
    - Endpoint を元に戻した
    - Ruby のロジックをちょっと見易くした

--- organism_related_disease_nano_stanza/stanza.rb
+++ organism_related_disease_nano_stanza/stanza.rb
@@ -1,6 +1,6 @@
 class OrganismRelatedDiseaseNanoStanza < TogoStanza::Stanza::Base
   property :list_disease do |tax_id|
-    results = query("http://ep.dbcls.jp/sparql7ssd", <<-SPARQL.strip_heredoc)
+    results = query("http://togostanza.org/sparql", <<-SPARQL.strip_heredoc)
       PREFIX pdo: <http://purl.jp/bio/11/pdo/>
       PREFIX tax: <http://identifiers.org/taxonomy/>
       SELECT DISTINCT ?pdo_id ?label
@@ -9,12 +9,11 @@ class OrganismRelatedDiseaseNanoStanza < TogoStanza::Stanza::Base
       WHERE {
         tax:#{tax_id} pdo:isAssociatedTo ?bk .
         ?bk ?p ?pdo_id .
-        ?pdo_id rdfs:label ?l
+        ?pdo_id rdfs:label ?label
         FILTER(regex(?pdo_id, "PDO"))
-        BIND(str(?l) AS ?label)
       }
     SPARQL
-    results[0] = "http://togostanza.org/static/pdo/pathogen.png" unless results == []
-    results
+
+    results.empty? ? '' : 'http://togostanza.org/static/pdo/pathogen.png'
   end
 end
