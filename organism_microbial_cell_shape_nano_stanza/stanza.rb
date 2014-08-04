5bc7576a5ab707515c60faeafb99951d5b79c9b9
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-08-04 17:48:17 +0900

    CellShape 画像のファイル名を求める SPARQL を変更した

--- organism_microbial_cell_shape_nano_stanza/stanza.rb
+++ organism_microbial_cell_shape_nano_stanza/stanza.rb
@@ -1,27 +1,42 @@
 class OrganismMicrobialCellShapeNanoStanza < TogoStanza::Stanza::Base
   property :cell_shapes do |tax_id|
     result = query("http://togostanza.org/sparql", <<-SPARQL.strip_heredoc).first
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      DEFINE sql:select-option "order"
+
       PREFIX mpo: <http://purl.jp/bio/01/mpo#>
       PREFIX tax: <http://identifiers.org/taxonomy/>
-      SELECT DISTINCT ?cell_shape ?label
-      FROM <http://togogenome.org/graph/mpo/>
+
+      SELECT ?label ?file_name
       FROM <http://togogenome.org/graph/gold/>
+      FROM <http://togogenome.org/graph/mpo/>
       WHERE {
-        tax:#{tax_id} mpo:MPO_10001 ?cell_shape .
-        ?cell_shape rdfs:label ?l .
-        FILTER (lang(?l) = "en")
-        BIND(str(?l) AS ?label)
+        VALUES ?arrange_label {
+          "Tetrad"@en "Diplococcus"@en "Diplobacillus"@en "Streptococcus"@en
+          "Streptobacillus"@en "Staphylococcus"@en "Single"@en
+        }
+        ?arrange rdfs:label ?arrange_label .
+        tax:#{tax_id} mpo:MPO_10016 ?arrange .
+        BIND (concat("arrangement_", lcase(?arrange_label), ".svg") AS ?arrange_file_name)
+
+        VALUES ?shape_label { "Rod"@en "Coccus"@en }
+        tax:#{tax_id} mpo:MPO_10001 ?shape .
+        ?shape rdfs:label ?shape_label .
+
+        BIND (IF (str(?shape_label) = "Rod", "Bacillus", ?shape_label) AS ?single_label)
+        BIND (concat("arrangement_", lcase(?single_label), ".svg") AS ?shape_file_name)
+
+        # arrange を使うか、 shape を使うかここで判断
+        BIND (IF (str(?arrange_label) = "Single", ?shape_file_name, ?arrange_file_name) AS ?file_name)
+        BIND (IF (str(?arrange_label) = "Single", ?shape_label, ?arrange_label) AS ?label)
       }
     SPARQL
 
     if result
-      result[:image_url] = ['http://togostanza.org/static/cell_shapes/shape_', result[:label].downcase, '.png'].join
+      result[:image_url] = "/stanza/assets/organism_microbial_cell_shape_nano/#{result[:file_name]}"
       result
     else
       {
         image_url: '/stanza/assets/no_data.svg',
-        cell_shape: '/stanza/assets/no_data.svg',
         label: 'No data'
       }
     end
